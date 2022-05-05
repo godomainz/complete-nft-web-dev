a6967b969a8b1084ff6e5a58d75764895cb1c364
Akila Mendis <akila@oneit.com.au>
2022-05-05 08:56:18 +0530

    35. Pragma Solidity (How To Select Compiler Versions)

--- solidity-crash-course/contracts/Crypto-Token-Contract.sol
+++ solidity-crash-course/contracts/Crypto-Token-Contract.sol
@@ -1,5 +1,7 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.4.22 <0.9.0;
+pragma solidity ^0.8.9;
+// ^ the uptick arrow defines a range compile version after the non zero
+// digit upwards to the next increment of the non zero number
 
 contract Coin {
  
