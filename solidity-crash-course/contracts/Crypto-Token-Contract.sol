82b0ea1fa20698a56402fb54d338fd4287afc533
Akila Mendis <akila@oneit.com.au>
2022-05-05 10:13:00 +0530

    37. What are Variables & Datatypes in Solidity

--- solidity-crash-course/contracts/Crypto-Token-Contract.sol
+++ solidity-crash-course/contracts/Crypto-Token-Contract.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.9;
+pragma solidity >=0.4.22 <0.9.0;
 // ^ the uptick arrow defines a range compile version after the non zero
 // digit upwards to the next increment of the non zero number
 
-contract Coin {
- 
+contract CryptoToken {
+    uint public tokens = 400;
 }
--- solidity-crash-course/migrations/1_initial_migration.js
+++ solidity-crash-course/migrations/1_initial_migration.js
@@ -1,8 +1,8 @@
 var Migrations = artifacts.require("Migrations");
-const Coin = artifacts.require("Coin");
+const CryptoToken = artifacts.require("CryptoToken");
 
 module.exports = function(deployer) {
   // Deploy the Migrations contract as our only task
   deployer.deploy(Migrations);
-  deployer.deploy(Coin);
+  deployer.deploy(CryptoToken);
 };
