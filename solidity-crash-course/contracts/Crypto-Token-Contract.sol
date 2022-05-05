da37fcf2a37751a13bfe7747442522a970bd95c4
Akila Mendis <akila@oneit.com.au>
2022-05-05 10:38:53 +0530

    41. Solution - Solidity Visibility Challenge

--- solidity-crash-course/contracts/Crypto-Token-Contract.sol
+++ solidity-crash-course/contracts/Crypto-Token-Contract.sol
@@ -4,6 +4,12 @@ pragma solidity >=0.4.22 <0.9.0;
 // digit upwards to the next increment of the non zero number
 
 contract CryptoToken {
-    uint private tokens = 400;
+    // uint private tokens = 400;
     address public minter;
+    // Exercise:
+    // 1. Create a var integer called totalSupply and set it to 7
+    // 2. totalSupply should not be accessible outside of the smart contract
+    // 3. create a var address called sender and set it so that it is publically accessible
+    /* uint private totalSupply = 7;
+    address public sender; */ 
 }
