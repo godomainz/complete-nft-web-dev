ab9bc6ae56f9f011a741f9a5ba39bd5937d7ad3e
Akila Mendis <akila@oneit.com.au>
2022-05-06 16:53:16 +0530

    50. Updating Token Balances & the += Operator

--- solidity-crash-course/contracts/Crypto-Token-Contract.sol
+++ solidity-crash-course/contracts/Crypto-Token-Contract.sol
@@ -1,19 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 <0.9.0;
-// ^ the uptick arrow defines a range compile version after the non zero
-// digit upwards to the next increment of the non zero number
 
 contract CryptoToken {
-    // Exercise:
-    // 1. Create a var integer called totalSupply and set it to 7
-    // 2. totalSupply should not be accessible outside of the smart contract
-    // 3. create a var address called sender and set it so that it is publically accessible
-    /* uint private totalSupply = 7;
-    address public sender; */
     mapping(address => uint) public balances;
     address public minter;
 
-    // constructor special type of dunction that get called immediately upon deployment
     constructor(){
         minter = msg.sender;
     }
@@ -21,5 +12,6 @@ contract CryptoToken {
     event Sent(address from, address to, uint amount);
     function mint(address receiver, uint amount) public {
         require(msg.sender == minter);
+        balances[receiver] += amount;
     }
 }
